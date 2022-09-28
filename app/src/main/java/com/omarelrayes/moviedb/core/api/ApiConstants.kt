f7f762b181e78902a50060fdb189b02e161af73b
Omar ELRayes <omarelreas@gmail.com>
2022-09-28 18:23:15 +0200

    Add utils and core network

---
+++ app/src/main/java/com/omarelrayes/moviedb/core/api/ApiConstants.kt
@@ -0,0 +1,6 @@
+package com.omarelrayes.moviedb.core.api
+
+object ApiConstants {
+    const val BASE_URL = "https://api.themoviedb.org/"
+    const val BASE_IMAGE_PATH = "https://image.tmdb.org/t/p/w185"
+}
---
+++ app/src/main/java/com/omarelrayes/moviedb/core/di/NetworkModule.kt
@@ -0,0 +1,65 @@
+package com.omarelrayes.moviedb.core.di
+
+import androidx.viewbinding.BuildConfig
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.omarelrayes.moviedb.core.api.ApiConstants
+import com.omarelrayes.moviedb.core.api.RequestInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val CONTENT_TYPE_VALUE = "application/json"
+
+    private val logger: HttpLoggingInterceptor
+        get() {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            if (BuildConfig.DEBUG) {
+                loggingInterceptor.apply { level = HttpLoggingInterceptor.Level.BODY }
+            }
+            return loggingInterceptor
+        }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(RequestInterceptor())
+            .addNetworkInterceptor(logger)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(ApiConstants.BASE_URL)
+            .addConverterFactory(getJsonConverterFactory())
+            .build()
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    private val json: Json
+        get() = Json {
+            ignoreUnknownKeys = true
+            explicitNulls = false
+        }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    private fun getJsonConverterFactory(): Converter.Factory {
+        return json.asConverterFactory(CONTENT_TYPE_VALUE.toMediaType())
+    }
+}
